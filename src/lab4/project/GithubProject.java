9618c2c4ea42a7f649d8241387852fd988d44e61
Jackhenry Masog <jack.masog@pcc.edu>
2022-02-13 02:44:41 -0800

    print date

--- src/lab4/project/GithubProject.java
+++ src/lab4/project/GithubProject.java
@@ -1,7 +1,8 @@
 package lab4.project;
-
+import java.util.Date;
 public class GithubProject {
     public static void main(String[] args) {
         System.out.println("Hello, Wold!");
+        System.out.println(new Date());
     }
 }
