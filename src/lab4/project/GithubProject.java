a6d8f16fffcbae5d49464b78d7154d5803aa85a0
Jackhenry Masog <jack.masog@pcc.edu>
2022-02-13 02:50:39 -0800

    added author and date

--- src/lab4/project/GithubProject.java
+++ src/lab4/project/GithubProject.java
@@ -1,5 +1,11 @@
 package lab4.project;
 import java.util.Date;
+
+/**
+ * @author Jackhenry Masog
+ * @version 02/13/2022
+ */
+
 public class GithubProject {
     public static void main(String[] args) {
         System.out.println("Hello, Wold!");
